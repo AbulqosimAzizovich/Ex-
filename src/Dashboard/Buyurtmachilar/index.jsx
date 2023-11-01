96aad44cd545203e040fef8e23327ee12d0141f1
TaN Taa <atan60896@gmail.com>
2023-11-01 11:42:39 +0500

    Dashboard design end

--- src/Dashboard/Buyurtmachilar/index.jsx
+++ src/Dashboard/Buyurtmachilar/index.jsx
@@ -1,8 +1,70 @@
 import React from "react";
-import "./style.scss";
+import { Link } from "react-router-dom";
+import { Space, Table, Tag, Breadcrumb } from "antd";
 
+import "./style.scss";
+import moment from "moment";
+
+const { Column, ColumnGroup } = Table;
+const data = [
+  {
+    key: "1",
+    number: "1",
+    fish: "Brown",
+    telephone: 32,
+    address: "Web site yaratish",
+    time: moment().format("YYYY-MM-DD HH:mm:ss"),
+  },
+  {
+    key: "2",
+    number: "2",
+    fish: "Green",
+    telephone: 42,
+    address: "Web site yaratish",
+    time: moment().format("YYYY-MM-DD HH:mm:ss"),
+  },
+  {
+    key: "3",
+    number: "3",
+    fish: "Black",
+    telephone: 32,
+    address: "Web site yaratish",
+    time: moment().format("YYYY-MM-DD HH:mm:ss"),
+  },
+];
 const index = () => {
-  return <div>Buyurtmachilar</div>;
+  return (
+    <div className="buyurtmachilar">
+      <div className="main__up">
+        <Breadcrumb
+          items={[
+            {
+              title: (
+                <Link to="/dashboard" className="text-[#000] ">
+                  Bosh sahifa
+                </Link>
+              ),
+            },
+            {
+              title: (
+                <Link to="/dashboard/buyurtmachilar" className="text-[#000] ">
+                  Buyurtmachilar
+                </Link>
+              ),
+            },
+          ]}
+        />
+      </div>
+
+      <Table dataSource={data}>
+        <Column title="#" dataIndex="number" key="number" />
+        <Column title="F.I.SH" dataIndex="fish" key="fish" />
+        <Column title="Telefon" dataIndex="telephone" key="telephone" />
+        <Column title="Xizmat" dataIndex="address" key="address" />
+        <Column title="Yaratilgan vaqti" dataIndex="time" key="time" />
+      </Table>
+    </div>
+  );
 };
 
 export default index;
--- src/Dashboard/Xizmatlar/index.jsx
+++ src/Dashboard/Xizmatlar/index.jsx
@@ -1,8 +1,221 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table, Button, Space, Input, Modal, Form, Breadcrumb } from "antd";
+import { v4 as uuidv4 } from "uuid";
+import { Link } from "react-router-dom";
+import moment from "moment";
 import "./style.scss";
 
 const index = () => {
-  return <div className="xizmat">Xizmatlar</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [data, setData] = useState([]);
+  const [order, setOrder] = useState("");
+  const [about, setAbout] = useState("");
+  const [name, setName] = useState("");
+  const [imageLink, setimageLink] = useState("");
+  const [editingRecord, setEditingRecord] = useState(null);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+
+  const handleAdd = () => {
+    const newData = [
+      ...data,
+      {
+        order,
+        about,
+        name,
+        imageLink,
+        time: moment().format("YYYY-MM-DD HH:mm:ss"),
+      },
+    ];
+    setData(newData);
+    setOrder("");
+    setAbout("");
+    setName("");
+    setimageLink("");
+  };
+
+  const handleEdit = (record) => {
+    setEditingRecord(record);
+    setOrder(record.order);
+    setAbout(record.about);
+    setName(record.name);
+    setimageLink(record.imageLink);
+    setEditModalVisible(true);
+  };
+
+  const handleSave = () => {
+    const updatedData = data.map((item) =>
+      item === editingRecord
+        ? {
+            ...item,
+            order,
+            about,
+            name,
+            imageLink,
+          }
+        : item
+    );
+    setData(updatedData);
+    setEditingRecord(null);
+    setOrder("");
+    setAbout("");
+    setName("");
+    setimageLink("");
+    setEditModalVisible(false);
+  };
+
+  const handleDelete = (record) => {
+    const newData = data.filter((item) => item !== record);
+    setData(newData);
+  };
+
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "order",
+      key: "order",
+    },
+    {
+      title: "Kurs Id",
+      dataIndex: "",
+      key: "",
+      render: (_, record) => <p>{uuidv4()} </p>,
+    },
+    {
+      title: "Kurs nomi",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Buyurtmachilar soni",
+      dataIndex: "",
+      key: "",
+    },
+    {
+      title: "Yaratilgan vaqti",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "Tahrirlash",
+      key: "action",
+      render: (_, record) => (
+        <Space>
+          <Button type="link" onClick={() => handleEdit(record)}>
+            Edit
+          </Button>
+          <Button type="link" onClick={() => handleDelete(record)}>
+            Delete
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div className="xizmatlar">
+      <div className="main__up2">
+        <Breadcrumb
+          items={[
+            {
+              title: (
+                <Link to="/dashboard" className="text-[#000] ">
+                  Bosh sahifa
+                </Link>
+              ),
+            },
+            {
+              title: (
+                <Link to="/dashboard/xizmatlar" className="text-[#000] ">
+                  Xizmatlar
+                </Link>
+              ),
+            },
+          ]}
+        />
+        <button onClick={showModal}>
+          <i className="bx bx-plus-circle text-[32px] text-[#287c36] hover:text-[#4dff6b]"></i>
+        </button>
+      </div>
+
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+        rowKey={(record) => record.about}
+      />
+      <Modal
+        title="Tahrirlash"
+        visible={editModalVisible}
+        onCancel={() => setEditModalVisible(false)}
+      >
+        <Form layout="vertical">
+          <Form.Item label="Tartib raqami">
+            <Input value={order} onChange={(e) => setOrder(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Kurs haqida">
+            <Input value={about} onChange={(e) => setAbout(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Kurs nomi">
+            <Input value={name} onChange={(e) => setName(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Rasm linki">
+            <Input
+              value={imageLink}
+              onChange={(e) => setimageLink(e.target.value)}
+            />
+          </Form.Item>
+
+          <button
+            className="px-4 py-1 rounded-[15px] border-[1px] border-[#00000034]"
+            onClick={() => handleSave()}
+          >
+            O'zgartirish
+          </button>
+        </Form>
+      </Modal>
+
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Space direction="vertical" style={{ marginBottom: 16, width: "100%" }}>
+          <Input
+            placeholder="Tartib raqami"
+            value={order}
+            onChange={(e) => setOrder(e.target.value)}
+          />
+
+          <Input
+            placeholder="Kurs nomi"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            placeholder="Kurs haqida"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+          />
+          <Input
+            placeholder="Rasm linki"
+            value={imageLink}
+            onChange={(e) => setimageLink(e.target.value)}
+          />
+          <Button onClick={handleAdd}>Add</Button>
+        </Space>
+      </Modal>
+    </div>
+  );
 };
 
 export default index;
