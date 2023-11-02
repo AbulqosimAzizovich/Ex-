9a09feed54a43cc7f323ebe91534d9ddfd1bb91e
TaN Taa <atan60896@gmail.com>
2023-11-03 04:16:25 +0500

    student status update

--- src/Dashboard/Oquvchilar/index.jsx
+++ src/Dashboard/Oquvchilar/index.jsx
@@ -108,7 +108,19 @@ const index = () => {
                             <td className="px-6 py-4">{e.phoneNumber}</td>
                             <td className="px-6 py-4">{e.courseId.title}</td>
                             <td className="px-6 py-4">{e.updatedAt}</td>
-                            <td className="px-6 py-4">Active</td>
+                            <td className="px-6 py-4">
+                              {e?.status == 3 ? (
+                                <><p className="text-red-500">O'qimaydi</p></>
+                              ) : e?.status == 2 ? (
+                                <><p className="text-green-500">O'qiydi</p></>
+                              ) : (
+                                <>
+                                  <p className="text-gray-700">
+                                    Ko'rib chiqimoqda
+                                  </p>
+                                </>
+                              )}
+                            </td>
                             <td className="px-6 py-4">
                               <button
                                 onClick={showModal}
